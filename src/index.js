a0ce341349ac307cc01d928804696bcb6481a12c
Anna <anjanadvorna@gmail.com>
2024-02-29 20:35:54 +0200

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/taskPro_project-group-6">
+      <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
