e98928b9cd8f8fdea19b5168977c062a400cc48a
Anna <anjanadvorna@gmail.com>
2024-03-01 05:10:51 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,11 @@
+
+import sprite from '../../assets/images/sprite.svg';
 export const App = () => {
   return (
     <div>
-      Welcome
+         <svg fill="transparent" width="18" height="18">
+            <use  stroke="black" href={`${sprite}#icon-trash-16`} />
+          </svg>
     </div>
   );
 };
