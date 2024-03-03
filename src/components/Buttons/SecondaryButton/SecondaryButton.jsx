c78ee08105840e8d15670f0846a0413f16e62ebc
Veronika Timakova <tymnik@ukr.net>
2024-03-03 14:09:54 +0200

    feat: secondary button component

--- src/components/Buttons/SecondaryButton/SecondaryButton.jsx
+++ src/components/Buttons/SecondaryButton/SecondaryButton.jsx
@@ -1,5 +1,14 @@
-const SecondaryButton = () => {
- 
-}
+import sprite from '../../../assets/images/sprite.svg';
+import css from './SecondaryButton.module.css';
+
+const SecondaryButton = ({ click }) => {
+  return (
+    <button className={css.button} onClick={click} type="submit">
+      <svg>
+        <use href={sprite + '#plus-20'}></use>
+      </svg>
+    </button>
+  );
+};
 
 export default SecondaryButton;
