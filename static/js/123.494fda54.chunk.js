f61158d004c7a2f77b8388422f11b0e7ca35be1a
AnnaNADVOR <101461125+AnnaNADVOR@users.noreply.github.com>
2024-03-03 15:30:53 +0000

    Deploying to gh-pages from @ AnnaNADVOR/taskPro_project-group-6@b04f57e6f22f5e7ba8e13536548a92c754a9c56f 🚀

--- static/js/123.494fda54.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[123],{123:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Welcome page"})}}}]);
-//# sourceMappingURL=123.494fda54.chunk.js.map
--- static/js/786.d788976c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[786],{786:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Auth page"})}}}]);
-//# sourceMappingURL=786.d788976c.chunk.js.map
