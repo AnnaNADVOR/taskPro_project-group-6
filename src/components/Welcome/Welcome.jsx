3637632735d766b0065724c833dd1c7414bcc59d
Viktoriya <arizona081392@gmail.com>
2024-03-04 17:58:31 +0200

    feature/welcomePage

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,41 @@
-const Welcome = () => {
- 
-}
+import { NavLink } from 'react-router-dom';
+import avatar  from '../../assets/images/logo-image-162.png';
+import sprite  from '../../assets/images/sprite.svg';
+import style from './Welcome.module.css'
+import Wrapper from 'components/Forms/AuthForms/Wrapper/Wrapper';
 
-export default Welcome;
+
+export default function Welcome() {
+    return (
+     
+        <Wrapper>
+    
+<div className={style.section}>
+   <img src={avatar} alt="Avatar" className={style.image} />
+      
+    <div className={style.box}>
+        <svg className={style.icon}>
+            <use href={`${sprite}#logo-black`} />
+          </svg>
+       <h1 className={style.title}>Task Pro</h1>
+    </div>
+      
+    <p className={style.text}>
+        Supercharge your productivity and take control of your tasks with Task
+        Pro - Don't wait, start achieving your goals now!
+    </p>
+      
+    <div className={style.linksBox}>
+        <NavLink to="auth/register" className={style.register}>
+          Registration
+        </NavLink>
+        <NavLink to="auth/login" className={style.login}>
+          Log In
+        </NavLink>
+    </div>
+
+</div>
+        
+        </Wrapper>
+  );
+}
