5fdd463c45cca034ceebbb30f7a834cac6fea430
Nina Pecherska <npecherska@gmail.com>
2024-03-04 09:44:41 +0200

    Add filter and start board

---
+++ src/components/BoardsList/HeaderDashboard/HeaderDashboard.jsx
@@ -0,0 +1,27 @@
+import Filter from 'components/Filter/Filter';
+import { useState } from 'react';
+import css from './HeaderDashboard.module.css';
+import sprite from 'assets/images/sprite.svg';
+import Modal from 'components/Modal/Modal';
+
+const Dashboard = () => {
+  const [isOpen, setIsOpen] = useState();
+  const toggleModal = () => {
+    setIsOpen(isOpen => !isOpen);
+  };
+
+  return (
+    <div className={css.container}>
+      <h2 className={css.projectTitle}>Name of project</h2>
+      <button className={css.buttonFilter} type=" button" onClick={toggleModal}>
+        <svg aria-label="filter icon" width="16" height="16">
+          <use className={css.filterIcon} href={`${sprite}#filter-16`}></use>
+        </svg>
+        <p className={css.filterTitle}>Filters</p>
+      </button>
+      {isOpen && <Modal closeModal={toggleModal} children={<Filter />} />}
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,5 +1,96 @@
-const Filter = () => {
- 
-}
+import { Formik, Form, Field } from 'formik';
+import css from './Filter.module.css';
+import { useState } from 'react';
 
+const Filter = () => {
+  const [isShowAllActive, setIsShowAllActive] = useState(false);
+
+  const handleChange = event => {
+    const filter = event.target.value;
+    console.log(filter);
+  };
+
+  const handleShowAll = () => {
+    setIsShowAllActive(true);
+  };
+
+  const handleRadioClick = () => {
+    if (isShowAllActive) {
+      setIsShowAllActive(false);
+    }
+  };
+  const initialValues = {
+    background: 0,
+    currentPrority: '',
+  };
+  return (
+    <div>
+      <h2 className={css.filterTitle}>Filters</h2>
+      <Formik initialValues={initialValues}>
+        <Form onChange={handleChange}>
+          <div>
+            <div className={css.filterWrap}>
+              <p className={css.textLeabel}>Label color</p>
+              <button
+                type="button"
+                onClick={handleShowAll}
+                disabled={isShowAllActive}
+                className={css.buttonLeabel}
+              >
+                Show all
+              </button>
+            </div>
+            <div className={css.leabelGroup}>
+              <label className={css.labelItems}>
+                <Field
+                  className={css.inputFormik}
+                  type="radio"
+                  name="priority"
+                  value="without"
+                  // color="#5b5b5b"
+                  // checked={priority === 'without'}
+                  onClick={handleRadioClick}
+                />
+                Without priority
+              </label>
+              <label className={css.labelItems}>
+                <Field
+                  type="radio"
+                  name="priority"
+                  value="low"
+                  // color="#8FA1D0"
+                  // checked={priority === 'low'}
+                  onClick={handleRadioClick}
+                />
+                Low
+              </label>
+              <label className={css.labelItems}>
+                <Field
+                  type="radio"
+                  name="priority"
+                  value="high"
+                  // color="#E09CB5"
+                  // checked={priority === 'medium'}
+                  onClick={handleRadioClick}
+                />
+                Medium
+              </label>
+              <label className={css.labelItems}>
+                <Field
+                  type="radio"
+                  name="priority"
+                  value="high"
+                  // color="#BEDBB0"
+                  // checked={priority === 'high'}
+                  onClick={handleRadioClick}
+                />
+                High
+              </label>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 export default Filter;
