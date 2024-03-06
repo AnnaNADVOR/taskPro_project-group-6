e5c6aa96da5c1085b807d5d4c57fc04d60a0908f
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2024-03-06 15:14:26 +0100

    add the component ColumnList in the ScreensPage

--- src/components/ScreensPage/Screens.page.jsx
+++ src/components/ScreensPage/Screens.page.jsx
@@ -1,12 +1,14 @@
 // import BoardStart from 'components/BoardsList/BoardStart/BoardStart';
 import AddBtnColumn from 'components/BoardsList/AddBtnColumn/AddBtnColumn';
 // import TestBtn from 'components/TestBtn/TestBtn';
+import ColumnsList from '../ColumnsList/ColumnsList';
 
 const ScreensPage = () => {
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: '20px', display: 'flex' }}>
       {/* <TestBtn /> */}
       {/* <BoardStart /> */}
+      <ColumnsList />
       <AddBtnColumn />
     </div>
   );
