263a4027368bac908d44e7babf51f0ab9fecc85c
Viktoriya <arizona081392@gmail.com>
2024-03-06 17:51:07 +0200

    bugdix
    var-color
    font-size error
    wrapper100vh
    bugfix-css

--- src/components/Forms/AuthForms/Schemas/Schemas.jsx
+++ src/components/Forms/AuthForms/Schemas/Schemas.jsx
@@ -22,10 +22,6 @@ const Schema = yup.object({
     .string()
     .trim()
     .required('Password is required')
-    .matches(
-      /^(?=.*[a-zA-Z0-9])[a-zA-Z0-9!@#$%^&*()-_=+[\]{}|;:',.<>?/~`]+$/,
-      'Invalid password format'
-    )
     .min(8, 'Password must be at least 8 characters')
     .max(64, 'Password must be no more than 64 characters')
     .test(
