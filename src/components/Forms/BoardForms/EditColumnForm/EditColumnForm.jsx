1b0b5e467642f06152acf87514fa35dd8804e518
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2024-03-06 15:13:51 +0100

    made and stylized the markup of the component EditColumnForm

--- src/components/Forms/BoardForms/EditColumnForm/EditColumnForm.jsx
+++ src/components/Forms/BoardForms/EditColumnForm/EditColumnForm.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import MainAddButton from 'components/Buttons/MainAddButton/MainAddButton';
+import css from './EditColumnForm.module.css';
+
 const EditColumnForm = () => {
- 
-}
+  const [title, setTitle] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setTitle(value);
+  };
+  return (
+    <div>
+      <h3 className={css.editColumnModalTitle}>Edit column</h3>
+      <form className={css.editColumnForm}>
+        <input
+          className={css.editColumnInput}
+          name="title"
+          value={title}
+          type="text"
+          placeholder="Title"
+          onChange={handleChange}
+          required
+        />
+        <MainAddButton text="Add" />
+      </form>
+    </div>
+  );
+};
 
 export default EditColumnForm;
