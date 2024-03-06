ffad151dffeace4fc699ad14da7bda955357272f
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2024-03-06 15:13:38 +0100

    made and stylized the markup of the component AddColumnForm

--- src/components/Forms/BoardForms/AddColumnForm/AddColumnForm.jsx
+++ src/components/Forms/BoardForms/AddColumnForm/AddColumnForm.jsx
@@ -1,5 +1,30 @@
+import { useState } from 'react';
+import MainAddButton from 'components/Buttons/MainAddButton/MainAddButton';
+import css from './AddColumnForm.module.css';
+
 const AddColumnForm = () => {
- 
-}
+  const [title, setTitle] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setTitle(value);
+  };
+  return (
+    <div>
+      <h3 className={css.addColumnModalTitle}>Add column</h3>
+      <form className={css.addColumnForm}>
+        <input
+          className={css.addColumnInput}
+          name="title"
+          value={title}
+          type="text"
+          placeholder="Title"
+          onChange={handleChange}
+          required
+        />
+        <MainAddButton text="Add" />
+      </form>
+    </div>
+  );
+};
 
 export default AddColumnForm;
