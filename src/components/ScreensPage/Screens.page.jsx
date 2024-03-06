748954c711d4143cdad7a7e70039b164997c06bc
Nina Pecherska <npecherska@gmail.com>
2024-03-06 19:27:58 +0200

    Update Screens.page.jsx

--- src/components/ScreensPage/Screens.page.jsx
+++ src/components/ScreensPage/Screens.page.jsx
@@ -5,7 +5,14 @@ import ColumnsList from '../ColumnsList/ColumnsList';
 
 const ScreensPage = () => {
   return (
-    <div style={{ padding: '20px', display: 'flex' }}>
+    <div
+      style={{
+        padding: '20px',
+        display: 'flex',
+        background: 'var( --main-bg-color)',
+        height: '93vh',
+      }}
+    >
       {/* <TestBtn /> */}
       {/* <BoardStart /> */}
       <ColumnsList />
