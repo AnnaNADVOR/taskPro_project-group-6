0290bf07480c0088bbe57b4d877e86ade9a66c67
Viktoriya <arizona081392@gmail.com>
2024-03-09 10:20:19 +0200

    login/register

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,6 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer } from 'redux-persist';
+import {
+    register,
+    logIn,
+    logOut,
+    refreshUser,
+} from './operation.js';
 
 const authSlice = createSlice({
     name: 'auth',
@@ -13,10 +19,61 @@ const authSlice = createSlice({
         isLoggedIn: false,
         isRefreshing: false,
     },
-    // extraReducers: builder => {
-    //     builder.addCase()
-    // },
-});
+    extraReducers: builder => {
+        builder
+            .addCase(register.pending, state => {
+                state.isLoadingRegister = true;
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoadingRegister = false; 
+                state.isLoggedIn = true; 
+                state.user = action.payload.user;
+                state.token = action.payload.token; 
+            })
+            .addCase(register.rejected, state => {
+                state.isLoadingRegister = false;                
+            })
+
+            .addCase(logIn.pending, state => {
+                state.isLoadingLogin = true;
+            })
+            .addCase(logIn.fulfilled, (state, action) => {
+                state.isLoadingLogin = false;                
+                state.isLoggedIn = true; 
+                state.user = action.payload.user;
+                state.token = action.payload.token; 
+            })
+            .addCase(logIn.rejected, state => {
+                state.isLoadingLogin = false;
+            })
+            
+            .addCase(logOut.pending, state => {
+                state.isLoadingLogout = true;
+            })
+            .addCase(logOut.fulfilled, state => {
+                state.isLoadingLogout = false;
+                state.isLoggedIn = false; 
+                state.user = { name: null, email: null };
+                state.token = null; 
+            })
+            .addCase(logOut.rejected, state => {
+                state.isLoadingLogout = false;
+            })
+
+            .addCase(refreshUser.pending, state => {
+                state.isRefreshing = true;
+            })
+            .addCase(refreshUser.fulfilled, (state, action) => {
+                state.user = action.payload;
+                state.isLoggedIn = true;
+                state.isRefreshing = false;                
+            })
+            .addCase(refreshUser.rejected, state => {
+                state.isRefreshing = false;
+            });
+    }
+}); 
+
 
 const persistConfig = {
     key: 'auth', 
