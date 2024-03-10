938eea02c94d049181adb06b45f2d1d2b4d780a6
Anna <anjanadvorna@gmail.com>
2024-03-10 21:25:16 +0200

    add filter

--- src/components/CardList/CradList.jsx
+++ src/components/CardList/CradList.jsx
@@ -1,18 +1,27 @@
 import { useSelector } from "react-redux";
-import { selectTasks } from "../../redux/tasks/selectors";
+import { selectTasks, selectVisibleTasks } from "../../redux/tasks/selectors";
 import Card from "../CardList/Card/Card";
 
 const CardList = () => {
     const cards = useSelector(selectTasks);
+    const visibleCards = useSelector(selectVisibleTasks);
+    console.log("cards",cards)
+    console.log("visible",visibleCards)
+    // const filtre = useSelector(selectFilter)
     return (
         <ul>
-            {cards.map(card =>
-                <li key={card._id}>
-                    <Card newCard={card} />
-                </li>
-            )}
+            {cards.length > 0 && visibleCards.length === 0 ? (console.log("no!")):
+           <>
+                {visibleCards.map(card =>
+                    <li key={card._id}>
+                        <Card newCard={card} />
+                    </li>
+                    )}
+            </>
+          
+            }
         </ul>
-    
+            
     )
 }
 
--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,11 +1,13 @@
 import { createSelector } from "@reduxjs/toolkit";
-import { selectFilter } from "redux/filter/selectors";
+import { selectFilter } from "../filter/selectors";
 
 export const selectTasks = state => state.tasks.tasksList;
 export const selectIsLoadingAdd = state => state.tasks.isLoadingAdd; 
 export const selectError = state => state.tasks.error; 
 export const selectDeletetaskId = state => state.tasks.deletetaskId;
-export const selectVibleTasks = createSelector([selectTasks, selectFilter], (tasks, filter) => {
+export const selectVisibleTasks = createSelector([selectTasks, selectFilter], (tasks, filter) => {
+    console.log("tasks", tasks)
+      console.log("filter",filter)
     const findTask = tasks.filter(task => task.priority.includes(filter));
     return findTask;
 })
