4e99064d9fb52aacea8a6ab78442777824992a6b
Anna <anjanadvorna@gmail.com>
2024-03-11 00:30:00 +0200

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -40,7 +40,7 @@ const authSlice = createSlice({
             .addCase(logIn.fulfilled, (state, action) => {
                 state.isLoadingLogin = false;                
                 state.isLoggedIn = true; 
-                state.user = action.payload;
+                state.user = action.payload.user;
                 state.token = action.payload.user.token; 
             })
             .addCase(logIn.rejected, state => {
