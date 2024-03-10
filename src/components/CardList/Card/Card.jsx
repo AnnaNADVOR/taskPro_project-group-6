fc97bc3f49f536a9e7a4bd38ae7b604af6959636
Anna <anjanadvorna@gmail.com>
2024-03-10 19:55:01 +0200

    ADD progress

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -10,18 +10,23 @@ import { deleteTask } from '../../../redux/tasks/operation';
 
 const Card = ({ newCard }) => {
   const [showModal, setShowModal] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-  const dispatch = useDispatch(); 
+  const toggleModal = () => setShowModal(prevShowModal => !prevShowModal);
+  const toggleMenu = () => setShowMenu(prevShowMenu => !prevShowMenu);
+  useEffect(() => {
+    const handleClickOutside = () => {
+      setShowMenu(false);
+    };
+    window.addEventListener('mousedown', handleClickOutside)
+  }, []);
 
+  const dispatch = useDispatch();
   const onDeleteTask = () => {
-        dispatch(deleteTask(newCard._id));           
+    dispatch(deleteTask(newCard._id));           
   }
   
-  const toggleModal = () => setShowModal(prevShowModal => !prevShowModal);
-
   const deadline = newCard.deadline.replace(/T.*/, '').split('-').reverse().join('/'); 
-
-  //priorityOptions
   const priority = newCard.priority;
   let color;
   switch (priority) {
@@ -29,10 +34,10 @@ const Card = ({ newCard }) => {
       color = 'var(--priority-low-color)';
       break;
     case 'Medium':
-      color = '#E09CB5';
+      color = 'var(--priority-medium-color)';
       break;
     case 'High':
-      color = '#BEDBB0';
+      color = 'var(--priority-high-color)';
       break;
     case "Without":
       color = 'var(--priority-color-without)';
@@ -40,44 +45,10 @@ const Card = ({ newCard }) => {
     default:
       color = `var(--priority-color-without)`;
   }
-
-  //progress popup
-  const [showMenu, setShowMenu] = useState(false);
-  const [setProgress] = useState('');
-
-  useEffect(() => {
-    const handleClickOutside = event => {
-      setShowMenu(false);
-    };
-
-    const handleKeyPress = event => {
-      if (event.key === 'Escape') {
-        setShowMenu(false);
-      }
-    };
-
-    document.addEventListener('mousedown', handleClickOutside);
-    document.addEventListener('keydown', handleKeyPress);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  const handleProgressChange = progress => {
-    setProgress(progress);
-    setShowMenu(false);
-  };
-
-  
+    
   return (
     <>
-      <div className={css.card} style={{ borderLeft: `4px solid ${color}` }}>
+    <div className={css.card} style={{ borderLeft: `4px solid ${color}` }}>
         <h4 className={css.cardTitle}>{newCard.title}</h4>
         <p className={css.cardDescription}>{newCard.description}</p>
         <div className={css.cardOptions}>
@@ -117,11 +88,7 @@ const Card = ({ newCard }) => {
               </button>
               <div className={css.progressContainer}>
                 {showMenu && (
-                  <Progress
-                    showMenu={showMenu}
-                    handleProgressChange={handleProgressChange}
-                    toggleMenu={toggleMenu}
-                  />
+                  <Progress closeMenu = {toggleMenu}/>
                 )}
               </div>
             </li>
--- src/components/CardList/Progress/Progress.jsx
+++ src/components/CardList/Progress/Progress.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import css from './Progress.module.css';
 import sprite from 'assets/images/sprite.svg';
-// import { useEffect } from "react"; 
+import { useEffect } from "react"; 
 
-const Progress = () => {
+const Progress = ({closeMenu}) => {
 
   const columnList = [
     { id: "27397520852ogegldknvdvdnlk", title: "To to" },
     { id: "327530janalal", title: "Done" },
     {id: "gjfjfkfelkfelkfwekwfelk1", title: "Cuttent"}
   ]
-  return (
 
-    <div className={css.popupMenu } onClick={onPopupClick}>
+  useEffect(() => {
+    const onEscClick = (event) => {
+      if (event.code === "Escape") {
+        closeMenu();
+      }
+    }
+
+    window.addEventListener("keydown", onEscClick);
+    return () => {
+      window.removeEventListener('keydown', onEscClick);
+    };  
+  }, [closeMenu]);
+  
+  return (
+    <div className={css.popupMenu}>
       <ul className={css.progressList}>
         {columnList.map(column => <li key={column.title}>
           <button className={css.progressListBtn}>
@@ -24,8 +37,7 @@ const Progress = () => {
         </li>)}         
       </ul>
     </div>
-    )
-
+  )
 };
 
 export default Progress;
