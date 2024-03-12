5bb46d5886020992892e9853e84ee0ba36615947
Veronika Timakova <tymnik@ukr.net>
2024-03-12 03:37:22 +0200

    bugfixes: board logic in slice, checking forms

--- src/components/SideBar/BoardList/BoardList.jsx
+++ src/components/SideBar/BoardList/BoardList.jsx
@@ -1,5 +1,4 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { NavLink, useLocation } from 'react-router-dom';
 
 import { getBoard } from '../../../redux/boards/operation';
 import { selectUser } from '../../../redux/auth/selectors';
@@ -8,28 +7,20 @@ import BoardListItem from '../BoardListItem/BoardListItem';
 import css from './BoardList.module.css';
 
 const BoardList = () => {
-  const location = useLocation();
-
   const { user } = useSelector(selectUser);
   const boards = user.boards;
   
   const dispatch = useDispatch();
 
   const handleClick = boardId => {
-    dispatch(getBoard(boardId));
+    dispatch(getBoard(boardId));    
   };
 
   return (
     <ul className={css.boardList}>
       {boards.map(board => (
-        <li key={board._id} onClick={() => handleClick(board._id)}>
-          <NavLink
-            to={`/home/${board.title}`}
-            state={{ from: location }}
-            className={css.navLink}
-          >
+        <li key={board._id} onClick={() => handleClick(board._id)}>          
             <BoardListItem board={board} allBoards={boards} />
-          </NavLink>
         </li>
       ))}
     </ul>
